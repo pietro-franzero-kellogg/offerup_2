cf7cc254d9c089cc4fbe7e272f283be9a4152c8f
anonymous anonymous <pietro.franzero@kellogg.northwestern.edu>
2022-02-21 04:11:28 +0000

    Generate graphiti resources for messages

---
+++ app/resources/message_resource.rb
@@ -0,0 +1,14 @@
+class MessageResource < ApplicationResource
+  attribute :id, :integer, writable: false
+  attribute :created_at, :datetime, writable: false
+  attribute :updated_at, :datetime, writable: false
+  attribute :negotiation_id, :integer
+  attribute :message, :string
+  attribute :sender_id, :integer
+  attribute :receiver_id, :integer
+
+  # Direct associations
+
+  # Indirect associations
+
+end
