b941e571a33c4bf29a13eebe50b7bbda66496dda
anonymous anonymous <pietro.franzero@kellogg.northwestern.edu>
2022-02-21 04:10:02 +0000

    Whitelist columns in Negotiation for ActiveAdmin

--- app/admin/negotiations.rb
+++ app/admin/negotiations.rb
@@ -1,5 +1,7 @@
 ActiveAdmin.register Negotiation do
 
+ permit_params :buyer_id, :seller_id, :item_id, :completed
+
   # See permitted parameters documentation:
   # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
   #
