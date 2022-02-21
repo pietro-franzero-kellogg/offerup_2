e1ddc4bc8a00d2fe4f1f56d74d807df9a12083b1
anonymous anonymous <pietro.franzero@kellogg.northwestern.edu>
2022-02-21 04:09:45 +0000

    Direct association between items and recommendations

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,6 +1,9 @@
 class Item < ApplicationRecord
   # Direct associations
 
+  has_many   :recommendations,
+             :dependent => :destroy
+
   belongs_to :user,
              :foreign_key => "seller_id"
 
