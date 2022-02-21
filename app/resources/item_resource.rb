78c4169deb3447ee9a4134babb520eb7e1b6144f
anonymous anonymous <pietro.franzero@kellogg.northwestern.edu>
2022-02-21 04:11:14 +0000

    Generate graphiti resources for items

---
+++ app/resources/item_resource.rb
@@ -0,0 +1,18 @@
+class ItemResource < ApplicationResource
+  attribute :id, :integer, writable: false
+  attribute :created_at, :datetime, writable: false
+  attribute :updated_at, :datetime, writable: false
+  attribute :name, :string
+  attribute :price, :float
+  attribute :description, :string
+  attribute :seller_id, :integer
+  attribute :image, :string
+  attribute :location, :string
+  attribute :category_id, :integer
+  attribute :available, :boolean
+
+  # Direct associations
+
+  # Indirect associations
+
+end
